9c8187b900dcf4cb6d146e3b0f369a5ebea56762
agaberr <ahmedgaber312001@gmail.com>
2025-04-26 20:05:26 +0300

    debubg = False

--- server/app.py
+++ server/app.py
@@ -497,4 +497,4 @@ if __name__ == '__main__':
     # from waitress import serve
     # serve(app, host="0.0.0.0", port=5000)
 
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
